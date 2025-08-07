1b5d484f16c0943356e959df9472c4658de15511
Yulo L. <noelleloyu@gmail.com>
2025-08-06 20:27:30 -0700

    Update 0003_longest_substring_without_repeating_characters.py

--- Leetcode/patterns/sliding_window/0003_longest_substring_without_repeating_characters.py
+++ Leetcode/patterns/sliding_window/0003_longest_substring_without_repeating_characters.py
@@ -1,5 +1,5 @@
 
-# Brute Force Approach
+# solution 1: Brute Force Approach
 # Time: O(n^2), Space: O(n)
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
@@ -20,7 +20,7 @@ class Solution:
         return max_len
 
 
-# Optimized Sliding Window using HashSet
+# solution 2: Optimized Sliding Window using HashSet
 # Time: O(n), Space: O(n)
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
@@ -37,3 +37,24 @@ class Solution:
             max_len = max(max_len, right - left + 1)  # Update max length
 
         return max_len  # Return result
+
+
+
+# solution 3: Sliding Window + HashMap
+# Time: O(n), Space: O(n)
+# Advantage: left pointer jumps directly to skip duplicates
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_index = {}
+        left = 0
+        max_len = 0
+
+        for right in range(len(s)):
+            if s[right] in char_index and char_index[s[right]] >= left:
+                left = char_index[s[right]] + 1
+            char_index[s[right]] = right
+            max_len = max(max_len, right - left + 1)
+        return max_len
+
+
